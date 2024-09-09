cf271038b5326ecd106b2de5ce44a6e875edbaf5
Med Amine bt <60607576+iifast2@users.noreply.github.com>
2024-09-09 20:27:17 +0100

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Odoo Users Limiter",
+    'version': '17.0.0.0',
+    'author': "Bytelegion",
+    'website': "http://medaminebt.com",
+    'depends': ['base'],
+    'license': "AGPL-3",
+    'category': "Tools",
+    'summary': "Limit the number of internal users created in the system.",
+    'description': """
+        The Users Limits module enables administrators to define a maximum limit for internal user creation. Once the limit is reached, new user creation is blocked. This restriction applies only to internal users, while portal and public users are unaffected.
+    """,
+    'data': ['views/res_users_view.xml'],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'images': ['static/description/banner.gif'],
+}
